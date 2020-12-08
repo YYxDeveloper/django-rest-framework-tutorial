6e513943ff570740509536db14eb409692d4760f
young <yyxdev@gmail.com>
2020-12-08 12:48:10 +0800

    設定限定json

--- DRF/musics/views.py
+++ DRF/musics/views.py
@@ -5,9 +5,12 @@ from musics.serializers import MusicSerializer
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
+from rest_framework.parsers import JSONParser
+
 
 # Create your views here.
 class MusicViewSet(viewsets.ModelViewSet):
     queryset = Music.objects.all()
     serializer_class = MusicSerializer
     permission_classes = (IsAuthenticated,)
+    # parser_classes = (JSONParser,)
