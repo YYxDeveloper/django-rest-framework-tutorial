85563e5ac186a2e04496ee54d86561c57b8c8ca1
young <yyxdev@gmail.com>
2020-12-09 16:42:36 +0800

    直接讀取sql at part5

--- DRF/musics/views.py
+++ DRF/musics/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 from django.shortcuts import get_object_or_404
 from musics.models import Music
-# from musics.models import fun_raw_sql_query, fun_sql_cursor_update
+from musics.models import fun_raw_sql_query, fun_sql_cursor_update
 from musics.serializers import MusicSerializer
 
 from rest_framework import viewsets, status
@@ -18,8 +18,9 @@ class MusicViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated,)
     parser_classes = (JSONParser,)
 
-    @action(methods=['get'], detail=True, permission_classes=[])
-    def detailaa(self, request, pk=None):
+    # /api/music/{pk}/detail/
+    @action(methods=['get'], detail=True)
+    def detail(self, request, pk=None):
         music = get_object_or_404(Music, pk=pk)
         result = {
             'singer': music.singer,
@@ -27,3 +28,35 @@ class MusicViewSet(viewsets.ModelViewSet):
         }
 
         return Response(result, status=status.HTTP_200_OK)
+
+    # /api/music/all_singer/
+    @action(methods=['get'], detail=False)
+    def all_singer(self, request):
+        music = Music.objects.values_list('singer', flat=True).distinct()
+        return Response(music, status=status.HTTP_200_OK)
+    #
+    # /api/music/raw_sql_query/
+    @action(methods=['get'], detail=False)
+    def raw_sql_query(self, request):
+        song = request.query_params.get('song', None)
+        music = fun_raw_sql_query(song=song)
+        serializer = MusicSerializer(music, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    #
+    # /api/music/{pk}/sql_cursor_update/
+    @action(methods=['put'], detail=True)
+    def sql_cursor_update(self, request, pk=None):
+        song = request.data.get('song', None)
+        if song:
+            music = fun_sql_cursor_update(song=song, pk=pk)
+            return Response(music, status=status.HTTP_200_OK)
+
+    # /api/music/version_api/
+    @action(methods=['get'], detail=False)
+    def version_api(self, request):
+        music = Music.objects.all()
+        if self.request.version == '1.0':
+            serializer = MusicSerializerV1(music, many=True)
+        else:
+            serializer = MusicSerializer(music, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
